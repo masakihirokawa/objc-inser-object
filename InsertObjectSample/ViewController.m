f3365c212b7f2d6e0c193d5a4620da0adaeccbb9
Masaki Hirokawa <info@dolice.net>
2013-03-29 22:03:07 +0900

    Add all files

---
+++ InsertObjectSample/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  InsertObjectSample
+//
+//  Created by Dolice on 2013/03/27.
+//  Copyright (c) 2013年 Dolice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self main];
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+}
+
+- (void)main
+{
+  //変更可能な配列を生成する
+  NSMutableArray *myArray;
+  myArray = [NSMutableArray arrayWithObject:@"Apple"];
+  //オブジェクトを挿入する
+  [myArray insertObject:@"Banana"
+                atIndex:0];
+  NSLog(@"%@", myArray);
+  
+  //変更可能な順序付けされたセットを生成する
+  NSMutableArray *orderdSet;
+  orderdSet = [NSMutableOrderedSet orderedSetWithObject:@"Apple"];
+  //オブジェクトを挿入する
+  [orderdSet insertObject:@"Banana"
+                  atIndex:0];
+  NSLog(@"%@", orderdSet);
+}
+
+@end
